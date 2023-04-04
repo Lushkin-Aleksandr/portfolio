09427ae3aedaee35ca7e7c079b57707bdafdf1fd
sanyalu <sanyalu97@gmail.com>
2023-04-04 19:15:26 +0200

    feat(Button): add loading effect to button

--- src/common/components/Button/Button.tsx
+++ src/common/components/Button/Button.tsx
@@ -1,23 +1,24 @@
-import React, { FC, MouseEvent } from 'react'
+import React, { ButtonHTMLAttributes, FC } from 'react'
 import { IconProp } from '@fortawesome/fontawesome-svg-core'
 import s from './Button.module.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRotate } from '@fortawesome/free-solid-svg-icons/faRotate'
 
-type PropsType = {
+type PropsType = ButtonHTMLAttributes<HTMLButtonElement> & {
   icon?: IconProp
+  loading?: boolean
   text: string
-  onClick: (e: MouseEvent<HTMLButtonElement>) => void
 }
 
-export const Button: FC<PropsType> = ({ icon, text, onClick }) => {
+export const Button: FC<PropsType> = ({ icon, text, loading, ...props }) => {
   const finalClass = icon ? `${s.btn} ${s.btnWithIcon}` : s.btn
 
   return (
-    <button className={finalClass} onClick={onClick}>
+    <button className={finalClass} {...props} disabled={loading}>
       {text}
       {icon && (
         <span className={s.btnIconHolder}>
-          <FontAwesomeIcon icon={icon} />
+          <FontAwesomeIcon icon={loading ? faRotate : icon} spin={loading} />
         </span>
       )}
     </button>
