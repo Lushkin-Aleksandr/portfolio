10c3a3296cd86e0b7687bd208fa39f32ef5a31ec
sanyalu <sanyalu97@gmail.com>
2023-04-11 16:01:09 +0200

    feat(PersonalInfo): add downloading CV

--- src/pages/AboutMe/PersonalInfo/PersonalInfo.tsx
+++ src/pages/AboutMe/PersonalInfo/PersonalInfo.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import s from './PersonalInfo.module.scss'
 import { InfoListItem } from './InfoListItem/InfoListItem'
 import { Button } from '../../../common/components/Button/Button'
 import { faDownload } from '@fortawesome/free-solid-svg-icons/faDownload'
+import cvPdf from '../../../assets/files/Lushkin_CV.pdf'
 
 export const PersonalInfo = () => {
+  const fileLinkRef = useRef<HTMLAnchorElement | null>(null)
+
+  const downloadCV = () => {
+    fileLinkRef.current?.click()
+  }
+
+  useEffect(() => {
+    const fileLink = document.createElement('a')
+    fileLink.href = cvPdf
+    fileLink.download = 'Lushkin_CV.pdf'
+
+    fileLinkRef.current = fileLink
+  }, [])
+
   return (
     <div className={s.personalInfo}>
       <h3 className={s.title}>personal info</h3>
@@ -17,7 +32,7 @@ export const PersonalInfo = () => {
         <InfoListItem title={'Languages'} value={'Russian, English(pre-intermediate)'} />
       </ul>
       <div className={s.btnWrapper}>
-        <Button text={'download cv'} onClick={() => {}} icon={faDownload} />
+        <Button text={'download cv'} onClick={downloadCV} icon={faDownload} />
       </div>
     </div>
   )
